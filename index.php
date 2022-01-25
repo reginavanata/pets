6291c6b92c4119671b5ada43c5cca6c8acbf2926
McZazz <91503026+McZazz@users.noreply.github.com>
2022-01-25 13:38:30 -0800

    views and index.php updated

--- index.php
+++ index.php
@@ -12,10 +12,9 @@ $f3 = Base::instance();
 
 //define a default route
 $f3->route('GET /', function() {
-    echo "<h1>Pet Home</h1>";
-
-    /*$view = new Template();
-    echo $view->render("views/home.html");*/
+    #echo "<h1>Pet Home</h1>";
+    $view = new Template();
+    echo $view->render("views/home.html");
 
 });
 
