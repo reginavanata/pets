75e082fad234888e6e37e4b5c3f849110ee8ce9d
rvanata <vanata.regina@student.greenriver.edu>
2022-01-25 16:25:46 -0500

    added controller page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+//create instance of Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function() {
+    echo "<h1>Pet Home</h1>";
+
+    /*$view = new Template();
+    echo $view->render("views/home.html");*/
+
+});
+
+//run fat-free
+$f3->run();
